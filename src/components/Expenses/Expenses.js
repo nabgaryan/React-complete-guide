d306b4b7d44e583e06b7387c96def18f93e86ae0
Norayr <norayr.abgaryan@gmail.com>
2022-08-09 17:22:36 +0400

    first commit

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -8,30 +8,23 @@ import { useState } from 'react';
 function Expenses(props) {
   const [enteredYear, setEnteredYear] = useState('2020');
 
-  const onSelectHandler = (selectedYear) =>{
+
+  const onSelectHandler = (selectedYear) => {
     setEnteredYear(selectedYear);
   };
-
+  const filteredExpenses = props.items.filter(expense => expense.date.getFullYear().toString() === enteredYear);
+console.log(filteredExpenses);
   return (
     <Card className="expenses">
-      <ExpensesFilter selected = {enteredYear} onselectYear = {onSelectHandler}/>
-      {props.items.mp}
-      <ExpenseItem
-        title={props.items[0].title}
-        amount={props.items[0].amount}
-        date={props.items[0].date}></ExpenseItem>
-      <ExpenseItem
-        title={props.items[1].title}
-        amount={props.items[1].amount}
-        date={props.items[1].date}></ExpenseItem>
-      <ExpenseItem
-        title={props.items[2].title}
-        amount={props.items[2].amount}
-        date={props.items[2].date}></ExpenseItem>
-      <ExpenseItem
-        title={props.items[3].title}
-        amount={props.items[3].amount}
-        date={props.items[3].date}></ExpenseItem>
+      <ExpensesFilter selected={enteredYear} onselectYear={onSelectHandler} />
+      {filteredExpenses.map(expense => (
+        <ExpenseItem
+          key={expense.id}
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+        />
+      ))}
     </Card>
   );
 
