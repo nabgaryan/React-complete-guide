13e416b791487b9254c50a9dac3789d91f1bee1c
Norayr <norayr.abgaryan@gmail.com>
2022-08-10 17:26:52 +0400

    econd commit

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,8 +1,8 @@
-import ExpenseItem from './ExpenseItem';
 import './Expenses.css';
 import Card from '../UI/Card';
 import ExpensesFilter from './ExpensesFilter';
 import { useState } from 'react';
+import ExpensesList from './ExpensesList';
 
 
 function Expenses(props) {
@@ -13,19 +13,15 @@ function Expenses(props) {
     setEnteredYear(selectedYear);
   };
   const filteredExpenses = props.items.filter(expense => expense.date.getFullYear().toString() === enteredYear);
-console.log(filteredExpenses);
-  return (
+  console.log(filteredExpenses);
+
+   return (
+    <li>
     <Card className="expenses">
       <ExpensesFilter selected={enteredYear} onselectYear={onSelectHandler} />
-      {filteredExpenses.map(expense => (
-        <ExpenseItem
-          key={expense.id}
-          title={expense.title}
-          amount={expense.amount}
-          date={expense.date}
-        />
-      ))}
+      <ExpensesList items={filteredExpenses}/>
     </Card>
+    </li>
   );
 
 }
